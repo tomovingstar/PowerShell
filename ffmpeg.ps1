a271f8089c4dd26524cd01a4f0262957f97288ab
Mike Kozlov <30219341+tomovingstar@users.noreply.github.com>
2018-05-06 17:56:33 -0500

    Add files via upload

---
+++ ffmpeg.ps1
@@ -0,0 +1,5 @@
+﻿C:\Users\nanoi\Downloads\ffmpeg-20180505-0803233-win64-static\ffmpeg-20180505-0803233-win64-static\bin\ffmpeg -f gdigrab -framerate 30 -i title=Calculator C:\Users\nanoi\Downloads\ffmpeg-20180505-0803233-win64-static\ffmpeg-20180505-0803233-win64-static\bin\output.mkv
+
+C:\Users\nanoi\Downloads\ffmpeg-20180505-0803233-win64-static\ffmpeg-20180505-0803233-win64-static\bin\ffmpeg -f gdigrab -framerate 30 -i desktop C:\Users\nanoi\Downloads\ffmpeg-20180505-0803233-win64-static\ffmpeg-20180505-0803233-win64-static\bin\output.mkv
+
+C:\Users\nanoi\Downloads\ffmpeg-20180505-0803233-win64-static\ffmpeg-20180505-0803233-win64-static\bin\ffmpeg -f gdigrab -framerate 30 -i desktop C:\Users\nanoi\Downloads\ffmpeg-20180505-0803233-win64-static\ffmpeg-20180505-0803233-win64-static\bin\output.mp4
